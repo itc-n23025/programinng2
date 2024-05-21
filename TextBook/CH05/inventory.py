2457e0613d53dda7f72c14c20f4397e6c6264610
Yuuya <n23025@std.it-college.ac.jp>
2024-05-21 09:40:17 +0000

    CH05

---
+++ TextBook/CH05/inventory.py
@@ -0,0 +1,10 @@
+stuff = {'ロープ': 1, 'たいまつ': 6, '金貨': 42, '手裏剣': 1, '矢': 12}
+
+def display_inventory(inventory):
+    print("  持ち物リスト:")
+    item_total = 0
+    for k, v inventory.item():
+
+    print("アイテム総数: " + str(item_total))
+
+display_inventory(stuff)
