8ed7b39cc5765641c1de5e397a39c4de4bef63a1
Yuuya <n23025@std.it-college.ac.jp>
2024-05-21 09:52:28 +0000

    add fin

---
+++ TextBook/CH05/addinv.py
@@ -0,0 +1,6 @@
+def add_to_inventory(inventory, added_items):
+
+inv = {'金貨': 42, 'ロープ': 1}
+dragon_loot = ['金貨', '手裏剣', '金貨', '金貨', 'ルビー']
+add_to_inventory(inv, dragon_loot)
+display_inventory(inv)
