972f294e302cd8ea296581e5d808b2cedbe7299a
Yuuya <n23025@std.it-college.ac.jp>
2024-06-28 06:35:44 +0000

    12章

---
+++ TextBook/CH12/play2048.py
@@ -0,0 +1,28 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
+import sys
+
+browser = webdriver.Chrome()
+browser.get('https://rugugu.jp/2048/')
+
+html = browser.find_element(By.TAG_NAME, 'html')
+
+print('停止するにはCTRL-Cを押してください。')
+
+try:
+    i = 1
+    while True:
+
+        if i % 2 == 1:
+            html.send_keys(Keys.UP)
+        elif i % 10 == 0:
+            html.send_keys(Keys.LEFT)
+        else:
+            html.send_keys(Keys.RIGHT)
+        time.sleep(1)
+        i += 1
+except KeyboardInterrupt:
+    print('終了')
+
